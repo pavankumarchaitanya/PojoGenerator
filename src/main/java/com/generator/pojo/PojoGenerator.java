b492146fa1ddcc7a0c6efa1ddacea85ac9178044
Pavan Kumar Chaitanya Landa <pavan.kumar.chaitanya@gmail.com>
2017-03-19 13:00:05 -0700

    initial commit

--- src/main/java/com/generator/pojo/PojoGenerator.java
+++ src/main/java/com/generator/pojo/PojoGenerator.java
@@ -1,9 +1,44 @@
+package com.generator.pojo;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class PojoGenerator {
+	Map<String, Set<Object>> qualifiedClassNameAndObjectsMap;
 
-	public static void main(String[] args) {
-System.out.println();
+	public void buildMap(Class clazz) throws ClassNotFoundException {
+
+		Set<Object> objectSet = qualifiedClassNameAndObjectsMap.get(clazz.getName());
+
+		if (objectSet != null) {
+			return;
+		}
+
+		Set<String> qualifiedClassNamesSet = getQualifiedClassNamesOfMemberVariables(clazz);
+
+		for (String className : qualifiedClassNamesSet) {
+
+			if (qualifiedClassNameAndObjectsMap.keySet().contains(className)) {
+				continue;
+			} else {
+				buildMap(Class.forName(className));
+			}
+		}
+
+		objectSet = generateObjectSetFromMap(qualifiedClassNameAndObjectsMap, clazz, new HashSet<String>(),
+				new HashSet<Object>());
+		qualifiedClassNameAndObjectsMap.put(clazz.getName(), objectSet);
 	}
 
-}
+	private Set<Object> generateObjectSetFromMap(Map<String, Set<Object>> qualifiedClassNameAndObjectsMap2, Class clazz,
+			HashSet<String> hashSet, HashSet<Object> hashSet2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	private Set<String> getQualifiedClassNamesOfMemberVariables(Class clazz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
